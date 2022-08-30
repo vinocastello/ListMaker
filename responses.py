ac5d77e1e95f0775f6b636a489341f97cfba7a2f
Hans De Castro <hhdecastro@up.edu.ph>
2022-08-30 22:36:31 +0800

    Included .env and added time command

--- responses.py
+++ responses.py
@@ -1,5 +1,6 @@
 import mao_quotes
 import weather
+from datetime import datetime
 
 def handle_response(command,sender) -> str:
     if command == 'hi' or command == 'hello':
@@ -8,7 +9,7 @@ def handle_response(command,sender) -> str:
     elif command == 'quotes' or command == 'quote':
         return mao_quotes.get_quote()
     elif command == 'tulong' or command == 'help':
-        return '''Use `$mao` as prefix for the commands\n`$mao hi` / `$mao hello` - greetings\n`$mao quotes` / `$mao quote` - random quote from comrade Mao\n`$mao tulong` / `$mao help` - details about commands\n`$mao calendar` - get the link of notion calendar'''
+        return '''Use `$mao` as prefix for the commands\n`$mao hi` / `$mao hello` - greetings\n`$mao quotes` / `$mao quote` - random quote from comrade Mao\n`$mao tulong` / `$mao help` - details about commands\n`$mao calendar` - get the link of notion calendar\n`$mao weather` - gives you weather update\n`$mao time` / `$mao date` - gives you date and time'''
     elif command == 'calendar' or command == 'petsa' or command == 'notion':
         return "https://www.notion.so/Calendar-c01b1b77cf2b4eee8333307be7f5b5f1"
     elif command == 'weather':
@@ -18,6 +19,10 @@ def handle_response(command,sender) -> str:
         else:
             loc = "Calamba, PH"
         return weather.get_weather(loc)
+    elif command == 'time' or command == 'date':
+        curr = datetime.now()
+        dt = curr.strftime("%d/%m/%Y %H:%M:%S")
+        return f"`{dt}`"
 
     #  return 'Yeah, I don\'t know. Try typing "!help".'
 
